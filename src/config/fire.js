195966ac069efc79f790d9885daf090022dfe295
cristianzabala08 <61285007+cristianzabala08@users.noreply.github.com>
2021-04-08 13:51:06 -0400

    Update fire.js

--- src/config/fire.js
+++ src/config/fire.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase'
 
 var config = {
-    apiKey: "AIzaSyB9mL746JkvW0Ngkx6rp2e-nFmSvQJNNCg",
-    authDomain: "my-bloger-8b202.firebaseapp.com",
-    databaseURL: "https://my-bloger-8b202.firebaseio.com",
-    projectId: "my-bloger-8b202",
-    storageBucket: "my-bloger-8b202.appspot.com",
-    messagingSenderId: "150722301128",
-    appId: "1:150722301128:web:c91b1134be56a9c9d469fd",
-    measurementId: "G-3S93M8S3KC"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
   var fire = firebase.initializeApp(config);
